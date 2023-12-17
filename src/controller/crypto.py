8d6deb10151c5ddd7aef6189903a66ae938d4003
Ahmed Dusuki <AhmedDusuki@protonmail.com>
2023-12-17 19:48:12 +0200

    Implemented Encrypt and Decrypt Added pycryptodomex as dependency (requirements.txt)

--- src/controller/crypto.py
+++ src/controller/crypto.py
@@ -1,5 +1,6 @@
 from argon2 import Type
 from argon2.low_level import hash_secret_raw
+from Cryptodome.Cipher import ChaCha20_Poly1305
 
 
 def kdf(password, salt):
@@ -12,3 +13,18 @@ def kdf(password, salt):
         hash_len=32,
         type=Type.ID,
     )
+
+
+def encrypt(plaintext, key, nonce, aad=b""):
+    cipher = ChaCha20_Poly1305.new(key=key, nonce=nonce)
+    cipher.update(aad)
+    ciphertext = cipher.encrypt(plaintext)
+    tag = cipher.digest()
+    return ciphertext, tag
+
+
+def decrypt(ciphertext, tag, key, nonce, aad=b""):
+    cipher = ChaCha20_Poly1305.new(key=key, nonce=nonce)
+    cipher.update(aad)
+    plaintext = cipher.decrypt_and_verify(ciphertext, tag)
+    return plaintext
