17c1c9fee4dc997209b6053b313ef7e14ec9a1af
Ahmed Dusuki <AhmedDusuki@protonmail.com>
2023-12-21 21:51:46 +0200

    Modified record and its tests so modification date is externally set

--- tests/test_record.py
+++ tests/test_record.py
@@ -1,9 +1,11 @@
 import pytest
 from src.model.record import Record
+from src.utils import TimeOracle
 import time
 
 
 class Test_Record:
+    time_oracle = TimeOracle()
     record = Record(
         1,
         "abcd",
@@ -11,39 +13,48 @@ class Test_Record:
         "work",
         "www.website.com",
         "the quick brown fox jumps over the lazy dog",
+        time_oracle.get_current_time(),
     )
 
-    def test_get_mdate(self, pass_mdate: bool = False):
-        assert 0 <= time.mktime(time.localtime()) - self.record.get_mdate(False) <= 2
+    @pytest.mark.parametrize("pass_mdate", [True])
+    def test_get_mdate(self, pass_mdate: bool):
+        assert 0 <= self.time_oracle.get_current_time() - self.record.record_mdate <= 2
         if pass_mdate:
-            assert self.record.get_mdate(False) == self.record.get_mdate(True)
+            assert self.record.record_mdate == self.record.pass_mdate
+        else:
+            assert self.record.record_mdate != self.record.pass_mdate
 
     def test_set_username(self):
         tests = ["a1b2c3", "t3stt3stt3st"]
         for username in tests:
             self.record.set_username(username)
-            self.test_get_mdate()
+            self.record.set_mdate(self.time_oracle.get_current_time())
+            self.test_get_mdate(pass_mdate=False)
             assert self.record.username == username
 
     def test_set_password(self):
         tests = ["admin123", "!)@&4c!&*!G(nD$"]
         for password in tests:
             self.record.set_password(password)
-            self.test_get_mdate(True)
+            self.record.set_mdate(self.time_oracle.get_current_time())
+            self.record.set_pass_mdate(self.record.record_mdate)
+            self.test_get_mdate(pass_mdate=True)
             assert self.record.password == password
 
     def test_set_tag(self):
         tests = ["social1", "financial"]
         for tag in tests:
             self.record.set_tag(tag)
-            self.test_get_mdate()
+            self.record.set_mdate(self.time_oracle.get_current_time())
+            self.test_get_mdate(pass_mdate=False)
             assert self.record.tag == tag
 
     def test_set_url(self):
         tests = ["www.a1b2c3.net", "t3stt3stt3st"]
         for url in tests:
             self.record.set_url(url)
-            self.test_get_mdate()
+            self.record.set_mdate(self.time_oracle.get_current_time())
+            self.test_get_mdate(pass_mdate=False)
             assert self.record.url == url
 
     def test_set_notes(self):
@@ -53,5 +64,6 @@ class Test_Record:
         ]
         for notes in tests:
             self.record.set_notes(notes)
-            self.test_get_mdate()
+            self.record.set_mdate(self.time_oracle.get_current_time())
+            self.test_get_mdate(pass_mdate=False)
             assert self.record.notes == notes
