fbc3502206ec73c98a0643ee25af783c0d1a06aa
Ahmed Dusuki <AhmedDusuki@protonmail.com>
2023-12-21 21:51:05 +0200

    Implemented TimeOracle for accurate, monotonic time

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import time
+
+
+class TimeOracle:
+    def __init__(self):
+        self.initial_perf_time = time.time() - time.perf_counter()
+
+    def get_current_time(self):
+        return self.initial_perf_time + time.perf_counter()
+
+    def get_readable_time(self, current_time):
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(current_time))
