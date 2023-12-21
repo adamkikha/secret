01394ad6caed28291c83485fe85adcdaa2d13386
Adam Kikha <adamkikha@yahoo.com>
2023-12-22 01:49:21 +0200

    Modified Record test to test PasswordRecord instead Renamed the file appropriately

--- tests/test_password_record.py
+++ tests/test_password_record.py
@@ -1,12 +1,11 @@
 import pytest
-from src.model.record import Record
+from src.model.password_record import PasswordRecord
 from src.utils import TimeOracle
-import time
 
 
-class Test_Record:
+class Test_PasswordRecord:
     time_oracle = TimeOracle()
-    record = Record(
+    pass_record = PasswordRecord(
         1,
         "abcd",
         "1234",
@@ -20,44 +19,48 @@ class Test_Record:
         reason="Initial call to function may be later than class creation by more than 2 seconds"
     )
     def test_get_mdate(self, pass_mdate: bool):
-        assert 0 <= self.time_oracle.get_current_time() - self.record.record_mdate <= 2
+        assert (
+            0
+            <= self.time_oracle.get_current_time() - self.pass_record.record_mdate
+            <= 2
+        )
         if pass_mdate:
-            assert self.record.record_mdate == self.record.pass_mdate
+            assert self.pass_record.record_mdate == self.pass_record.pass_mdate
         else:
-            assert self.record.record_mdate != self.record.pass_mdate
+            assert self.pass_record.record_mdate != self.pass_record.pass_mdate
 
     def test_set_username(self):
         tests = ["a1b2c3", "t3stt3stt3st"]
         for username in tests:
-            self.record.set_username(username)
-            self.record.set_mdate(self.time_oracle.get_current_time())
+            self.pass_record.set_username(username)
+            self.pass_record.set_mdate(self.time_oracle.get_current_time())
             self.test_get_mdate(pass_mdate=False)
-            assert self.record.username == username
+            assert self.pass_record.username == username
 
     def test_set_password(self):
         tests = ["admin123", "!)@&4c!&*!G(nD$"]
         for password in tests:
-            self.record.set_password(password)
-            self.record.set_mdate(self.time_oracle.get_current_time())
-            self.record.set_pass_mdate(self.record.record_mdate)
+            self.pass_record.set_password(password)
+            self.pass_record.set_mdate(self.time_oracle.get_current_time())
+            self.pass_record.set_pass_mdate(self.pass_record.record_mdate)
             self.test_get_mdate(pass_mdate=True)
-            assert self.record.password == password
+            assert self.pass_record.password == password
 
     def test_set_tag(self):
         tests = ["social1", "financial"]
         for tag in tests:
-            self.record.set_tag(tag)
-            self.record.set_mdate(self.time_oracle.get_current_time())
+            self.pass_record.set_tag(tag)
+            self.pass_record.set_mdate(self.time_oracle.get_current_time())
             self.test_get_mdate(pass_mdate=False)
-            assert self.record.tag == tag
+            assert self.pass_record.tag == tag
 
     def test_set_url(self):
         tests = ["www.a1b2c3.net", "t3stt3stt3st"]
         for url in tests:
-            self.record.set_url(url)
-            self.record.set_mdate(self.time_oracle.get_current_time())
+            self.pass_record.set_url(url)
+            self.pass_record.set_mdate(self.time_oracle.get_current_time())
             self.test_get_mdate(pass_mdate=False)
-            assert self.record.url == url
+            assert self.pass_record.url == url
 
     def test_set_notes(self):
         tests = [
@@ -65,7 +68,7 @@ class Test_Record:
             "Cras ullamcorper cursus tincidunt. Curabitur a cursus quam, at egestas nisi. Nam dignissim, libero at",
         ]
         for notes in tests:
-            self.record.set_notes(notes)
-            self.record.set_mdate(self.time_oracle.get_current_time())
+            self.pass_record.set_notes(notes)
+            self.pass_record.set_mdate(self.time_oracle.get_current_time())
             self.test_get_mdate(pass_mdate=False)
-            assert self.record.notes == notes
+            assert self.pass_record.notes == notes
