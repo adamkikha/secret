c6434eb2e6882ffac50712b12352f533b7072e23
Ahmed Dusuki <AhmedDusuki@protonmail.com>
2023-12-22 00:19:49 +0200

    Implemented Controller

---
+++ src/controller/controller.py
@@ -0,0 +1,67 @@
+import os
+from src.controller.crypto import kdf, encrypt, decrypt
+
+
+class Controller:
+    def __init__(self):
+        self.file_path = None
+
+    def set_model(self, model):
+        self.model = model
+
+    def create_file(self, master_key, file_path):
+        self.file_path = file_path
+        self.model.initialize(file_path, create=True)
+        self.salt = os.urandom(16)
+        self.derived_key = kdf(master_key, self.salt)
+        self.nonce = os.urandom(24)
+        aad = self.nonce + self.salt  # concatenate nonce and salt
+        self.ciphertext, self.tag = encrypt(
+            self.model.serialize_records(), self.derived_key, self.nonce, aad=aad
+        )
+        self.model.save_file(self.ciphertext, self.tag, self.nonce, self.salt)
+
+    def open_file(self, master_key, file_path):
+        self.file_path = file_path
+        try:
+            self.model.initialize(file_path, create=False)
+        except Exception as e:
+            self.file_path = None
+            print(f"Opening file failed: {e}")
+            return False
+        self.salt = self.model.salt
+        self.derived_key = kdf(master_key, self.salt)
+        self.nonce = self.model.nonce
+        self.ciphertext = self.model.ciphertext
+        self.tag = self.model.tag
+        aad = self.nonce + self.salt  # concatenate nonce and salt
+        try:
+            self.plaintext = decrypt(
+                self.ciphertext, self.tag, self.derived_key, self.nonce, aad
+            )
+        except Exception as e:
+            self.file_path = None
+            print(f"Decryption failed: {e}")
+            return False
+        self.model.construct_records(self.plaintext)
+        self.plaintext = None
+        return True
+
+    def save_file(self):
+        if self.file_path:
+            self.nonce = os.urandom(24)
+            aad = self.nonce + self.salt
+            self.ciphertext, self.tag = encrypt(
+                self.model.serialize_records(), self.derived_key, self.nonce, aad=aad
+            )
+            self.model.save_file(self.ciphertext, self.tag, self.nonce, self.salt)
+            return True
+        else:
+            return False
+
+    def close_file(self, save: bool):
+        if save:
+            self.save_file()
+        self.derived_key = None
+        self.file_path = None
+        self.model.close_file()
