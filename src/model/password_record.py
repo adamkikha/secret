09daf09cdcb639e26e53067d5f87850e98e304eb
Adam Kikha <adamkikha@yahoo.com>
2023-12-22 02:02:25 +0200

    Split Record class into Record and PasswordRecord classes

---
+++ src/model/password_record.py
@@ -0,0 +1,31 @@
+from src.model.record import Record
+
+
+class PasswordRecord(Record):
+    def __init__(
+        self,
+        id: int,
+        username: str,
+        password: str,
+        tag: str,
+        url: str,
+        notes: str,
+        date: float,
+    ):
+        super().__init__(id, tag, notes, date)
+        self.username = username
+        self.password = password
+        self.url = url
+        self.pass_mdate = date
+
+    def set_username(self, username: str):
+        self.username = username
+
+    def set_password(self, password: str):
+        self.password = password
+
+    def set_url(self, url: str):
+        self.url = url
+
+    def set_pass_mdate(self, date: float):
+        self.pass_mdate = date
--- src/model/record.py
+++ src/model/record.py
@@ -1,43 +1,21 @@
-import time
-
-
 class Record:
     def __init__(
         self,
         id: int,
-        username: str,
-        password: str,
         tag: str,
-        url: str,
         notes: str,
         date: float,
     ):
         self.id = id
-        self.username = username
-        self.password = password
         self.tag = tag
-        self.url = url
         self.notes = notes
         self.record_mdate = date
-        self.pass_mdate = date
-
-    def set_username(self, username: str):
-        self.username = username
-
-    def set_password(self, password: str):
-        self.password = password
 
     def set_tag(self, tag: str):
         self.tag = tag
-
-    def set_url(self, url: str):
-        self.url = url
 
     def set_notes(self, notes: str):
         self.notes = notes
 
     def set_mdate(self, date: float):
         self.record_mdate = date
-
-    def set_pass_mdate(self, date: float):
-        self.pass_mdate = date
