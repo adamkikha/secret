fb7d70b0d68e41a9ee5e5e7b421ba9413e9254e2
Ahmed Dusuki <AhmedDusuki@protonmail.com>
2023-12-25 06:49:41 +0200

    Implemented TimeOracle update_model

--- src/utils.py
+++ src/utils.py
@@ -4,10 +4,26 @@ import time
 class TimeOracle:
     def __init__(self):
         self.initial_perf_time = time.time() - time.perf_counter()
+        self.model = None
 
     def get_current_time(self):
         return self.initial_perf_time + time.perf_counter()
-    
+
     @staticmethod
     def get_readable_time(current_time):
         return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(current_time))
+
+    def set_model(self, model):
+        self.model = model
+
+    def update_model(self):
+        if not self.model:
+            raise RuntimeError
+
+        while True:
+            time.sleep(60)
+            with self.model.update_data_lock:
+                try:
+                    self.model.update_data()
+                except RuntimeError:
+                    break
