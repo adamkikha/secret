54708183106e73a81f5c2c897302b45074af3d2e
Adam Kikha <adamkikha@yahoo.com>
2023-12-26 19:50:35 +0200

    Implemented Settings Class

---
+++ src/model/password_settings.py
@@ -0,0 +1,10 @@
+class PasswordSettings:
+    def __init__(self):
+        self.warn = True
+        self.warn_age = 90 * 24 * 60 * 60
+        self.saved_version_count = 1
+        self.lower_case = True
+        self.upper_case = True
+        self.symbols = True
+        self.digits = True
+        self.length = 26
